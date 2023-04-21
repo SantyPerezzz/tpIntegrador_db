6792a5f384e0396fcf7b1ce73655c8dc1f6fba79
a-Fernando <almaraz.fernando.m@gmail.com>
2023-04-21 20:57:09 -0300

    Cambiaron los strings con los datos de configuraciones por un arrayList

--- src/main/java/programa/Funciones.java
+++ src/main/java/programa/Funciones.java
@@ -10,227 +10,237 @@ import fulbo.Partido.Resultado;
 import java.sql.*;
 
 public class Funciones {
-	
-	public static Partido buscarPartido(ArrayList<Partido> partidos,Equipo team1, Equipo team2) {
-		Partido aux= new Partido();
-		for(Partido part: partidos) {
-			if((part.getEquipo1()==team1 || part.getEquipo2()==team1)&&(part.getEquipo1()==team2 || part.getEquipo2()==team2)) {
-				aux=part;
+
+	public static Partido buscarPartido(ArrayList<Partido> partidos, Equipo team1, Equipo team2) {
+		Partido aux = new Partido();
+		for (Partido part : partidos) {
+			if ((part.getEquipo1() == team1 || part.getEquipo2() == team1)
+					&& (part.getEquipo1() == team2 || part.getEquipo2() == team2)) {
+				aux = part;
 			}
 		}
-		
+
 		return aux;
 	}
-	
-	public static int posEquipo(ArrayList<Equipo> equipos,String nombre) {
-		int pos=-1;
-		for(Equipo eq:equipos) {
-			if(eq.getNombre().equals(nombre)) {
-				pos=equipos.indexOf(eq);
+
+	public static int posEquipo(ArrayList<Equipo> equipos, String nombre) {
+		int pos = -1;
+		for (Equipo eq : equipos) {
+			if (eq.getNombre().equals(nombre)) {
+				pos = equipos.indexOf(eq);
 			}
 		}
-		
+
 		return pos;
 	}
-	
-	public static int posParticipante(ArrayList<Participante> participantes,String nombre) {
-		int pos=-1;
-		for(Participante part:participantes) {
-			if(part.getNombre().equals(nombre)) {
-				pos=participantes.indexOf(part);
+
+	public static int posParticipante(ArrayList<Participante> participantes, String nombre) {
+		int pos = -1;
+		for (Participante part : participantes) {
+			if (part.getNombre().equals(nombre)) {
+				pos = participantes.indexOf(part);
 			}
 		}
-		
+
 		return pos;
 	}
-	
-	public static int posRonda(ArrayList<Ronda> rondas,String nro) {
-		int pos=-1;
-		for(Ronda r:rondas) {
-			if(r.getNro().equals(nro)) {
-				pos=rondas.indexOf(r);
+
+	public static int posRonda(ArrayList<Ronda> rondas, String nro) {
+		int pos = -1;
+		for (Ronda r : rondas) {
+			if (r.getNro().equals(nro)) {
+				pos = rondas.indexOf(r);
 			}
 		}
-		
+
 		return pos;
 	}
-	
+
 	public static Resultado resEquipo1(int resultado) {
-		Resultado res=Resultado.perdio;
-		if(resultado==1) {
-			res= Resultado.gano;
-		}else if(resultado==0) {
-			res= Resultado.empato;
+		Resultado res = Resultado.perdio;
+		if (resultado == 1) {
+			res = Resultado.gano;
+		} else if (resultado == 0) {
+			res = Resultado.empato;
 		}
-		
+
 		return res;
 	}
-	
+
 	public static String stringVs(ArrayList<Partido> partidos) {
-		String partidosString="";
-		for(Partido p:partidos) {
-			partidosString+=p.getEquipo1().getNombre()+" vs "+p.getEquipo2().getNombre()+"\n";
+		String partidosString = "";
+		for (Partido p : partidos) {
+			partidosString += p.getEquipo1().getNombre() + " vs " + p.getEquipo2().getNombre() + "\n";
 		}
 		return partidosString;
 	}
-	
+
 	public static boolean verificarLineaResultado(String linea) {
-		boolean b=false;
-		String[] lineaSpliteada= linea.split(",");
-		if(lineaSpliteada.length==5) {
-			if(Integer.parseInt(lineaSpliteada[2])>=0) {
-				if(Integer.parseInt(lineaSpliteada[3])>=0) {
-					b=true;
+		boolean b = false;
+		String[] lineaSpliteada = linea.split(",");
+		if (lineaSpliteada.length == 5) {
+			if (Integer.parseInt(lineaSpliteada[2]) >= 0) {
+				if (Integer.parseInt(lineaSpliteada[3]) >= 0) {
+					b = true;
 				}
 			}
 		}
-		
+
 		return b;
 	}
-	
-	public static String[] leerArchivoConfiguraciones(Path configuracionesPath,String dbUrl,String dbUser,String dbPw,Integer puntosPorAcierto,Integer puntosExtra) {
-		String[] arreglo= new String[5];
-		
-		try{
-			List<String> aux=Files.readAllLines(configuracionesPath);
-			arreglo[0]=aux.get(0);
-			arreglo[1]=aux.get(1);
-			arreglo[2]=aux.get(2);
-			arreglo[3]=aux.get(3);
-			arreglo[4]=aux.get(4);
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
-		dbUrl=arreglo[0];
-		return arreglo;
-	}
-	
-	public static void leerArchivoResultados(Path resultadosPath,ArrayList<Ronda> rondas,ArrayList<Partido> partidos,ArrayList<Equipo> equipos) {
+
+	public static void leerArchivoConfiguraciones(Path configuracionesPath, ArrayList<String> configuraciones,
+			Integer puntosPorAcierto, Integer puntosExtra) {
+		int i = 0;
 		try {
-			for (String linea : Files.readAllLines(resultadosPath)) {
-				if(verificarLineaResultado(linea)) {
-		        	String nroRonda= linea.split(",")[0];
-		        	Ronda auxRonda= new Ronda(nroRonda);
-		        	if(posRonda(rondas,nroRonda)!=-1) {
-		        		auxRonda=rondas.get(posRonda(rondas,nroRonda));
-		        	}else {
-		        		rondas.add(auxRonda);
-		        	}
-		        	
-		        	String nomEq1=linea.split(",")[1];
-		        	String nomEq2=linea.split(",")[4];
-		        	Equipo aux1 = new Equipo(nomEq1, "");
-		            if(posEquipo(equipos, nomEq1)!=-1) {
-		            	aux1 = equipos.get(posEquipo(equipos,nomEq1));	
-		            }else {
-		            	equipos.add(aux1);
-		            }
-		            Equipo aux2 = new Equipo(nomEq2, "");
-		            if(posEquipo(equipos,nomEq2)!=-1) {
-		            	aux2= equipos.get(posEquipo(equipos,nomEq2));
-		            }else {
-		            	equipos.add(aux2);
-		            }
-		            Partido auxPart= new Partido(aux1, aux2, Integer.parseInt(linea.split(",")[2]),Integer.parseInt(linea.split(",")[3]));
-		            partidos.add(auxPart);
-		            auxRonda.agregarPartido(auxPart);
-		        }
+			List<String> aux = Files.readAllLines(configuracionesPath);
+			while (i < 5) {
+				configuraciones.add(aux.get(i));
+				i++;
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
-	}
-	
-	/*public static void leerArchivoPronosticos(Path pronosticoPath,ArrayList<Participante> participantes,ArrayList<Equipo> equipos,ArrayList<Partido> partidos) {
-		try {
-			for(String linea: Files.readAllLines(pronosticoPath)) {
-            	String nomPart= linea.split(",")[0];
-            	Participante auxPart= new Participante(nomPart);
-            	if(posParticipante(participantes,nomPart)!=-1) {
-            		auxPart=participantes.get(posParticipante(participantes, nomPart));
-            	}else {
-            		participantes.add(auxPart);
-            	}
-            	
-            	String nomEq1= linea.split(",")[1];
-            	String nomEq2= linea.split(",")[5];
-            	Equipo eqAux1= equipos.get(posEquipo(equipos, nomEq1));
-            	Equipo eqAux2= equipos.get(posEquipo(equipos,nomEq2));
-            	
-            	Pronostico aux = new Pronostico(buscarPartido(partidos,eqAux1,eqAux2),eqAux1,resEquipo1(linea.split(",")));
-            	
-            	auxPart.agregarPronostico(aux);
-            }
-		}catch(Exception e) {
-			System.out.println(e);
-		}
-		
-	}*/
-	
-	public static void leerBaseDatosPronosticos(String dbUrl,String dbUser,String dbPw,ArrayList<Participante> participantes,ArrayList<Equipo> equipos,ArrayList<Partido> partidos) {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con= DriverManager.getConnection(dbUrl,dbUser,dbPw);
-			Statement stmt= con.createStatement();
-			
-			ResultSet rs= stmt.executeQuery("select participante,equipo1,resultado,equipo2 from pronosticos");
-			while(rs.next()) {
-				String nomPart= rs.getString(1);
-				Participante auxPart= new Participante(nomPart);
-            	if(posParticipante(participantes,nomPart)!=-1) {
-            		auxPart=participantes.get(posParticipante(participantes, nomPart));
-            	}else {
-            		participantes.add(auxPart);
-            	}
-            	String nomEq1= rs.getString(2);
-            	String nomEq2= rs.getString(4);
-            	Equipo eqAux1= equipos.get(posEquipo(equipos, nomEq1));
-            	Equipo eqAux2= equipos.get(posEquipo(equipos,nomEq2));
-            	
-            	Pronostico aux = new Pronostico(buscarPartido(partidos,eqAux1,eqAux2),eqAux1,resEquipo1(rs.getInt(3)));
-            	
-            	auxPart.agregarPronostico(aux);
-			}
-			
-			
-			con.close();
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
-		
-	}
-	
-	
-	public static boolean acertoPronosticosDeRonda(Participante part,Ronda ronda) {
-		boolean r=true;
-		ArrayList<Partido> partidosAcertados= new ArrayList<Partido>();
-		for(Pronostico p:part.pronosticosAcertados()) {
-			partidosAcertados.add(p.getPartido());
-		}
-		for(Partido p:ronda.getPartidos()) {
-			if(!partidosAcertados.contains(p)) {
-				r=false;
-			}
-		}
-		
-		return r;
-	}
-	
-	public static void mostrarParticipante(Participante p) {
-		System.out.println("Participante: "+p.getNombre()+"\t"+"Puntos: "+p.puntosTotales()+"\t"+"Pronosticos acertados: "+p.pronosticosAcertados().size());
 	}
 
-	/*public static boolean nombreADefinir(ArrayList<Ronda> rondas,Participante part, String nroRonda) {
-		boolean r=true;
-		Ronda auxRonda= new Ronda("0");
-		for(Ronda ron:rondas) {
-			if(ron.getNro().equals(nroRonda)) {
-				auxRonda=ron;
+	public static void leerArchivoResultados(Path resultadosPath, ArrayList<Ronda> rondas, ArrayList<Partido> partidos,
+			ArrayList<Equipo> equipos) {
+		try {
+			for (String linea : Files.readAllLines(resultadosPath)) {
+				if (verificarLineaResultado(linea)) {
+					String nroRonda = linea.split(",")[0];
+					Ronda auxRonda = new Ronda(nroRonda);
+					if (posRonda(rondas, nroRonda) != -1) {
+						auxRonda = rondas.get(posRonda(rondas, nroRonda));
+					} else {
+						rondas.add(auxRonda);
+					}
+
+					String nomEq1 = linea.split(",")[1];
+					String nomEq2 = linea.split(",")[4];
+					Equipo aux1 = new Equipo(nomEq1, "");
+					if (posEquipo(equipos, nomEq1) != -1) {
+						aux1 = equipos.get(posEquipo(equipos, nomEq1));
+					} else {
+						equipos.add(aux1);
+					}
+					Equipo aux2 = new Equipo(nomEq2, "");
+					if (posEquipo(equipos, nomEq2) != -1) {
+						aux2 = equipos.get(posEquipo(equipos, nomEq2));
+					} else {
+						equipos.add(aux2);
+					}
+					Partido auxPart = new Partido(aux1, aux2, Integer.parseInt(linea.split(",")[2]),
+							Integer.parseInt(linea.split(",")[3]));
+					partidos.add(auxPart);
+					auxRonda.agregarPartido(auxPart);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/*
+	 * public static void leerArchivoPronosticos(Path
+	 * pronosticoPath,ArrayList<Participante> participantes,ArrayList<Equipo>
+	 * equipos,ArrayList<Partido> partidos) {
+	 * try {
+	 * for(String linea: Files.readAllLines(pronosticoPath)) {
+	 * String nomPart= linea.split(",")[0];
+	 * Participante auxPart= new Participante(nomPart);
+	 * if(posParticipante(participantes,nomPart)!=-1) {
+	 * auxPart=participantes.get(posParticipante(participantes, nomPart));
+	 * }else {
+	 * participantes.add(auxPart);
+	 * }
+	 * 
+	 * String nomEq1= linea.split(",")[1];
+	 * String nomEq2= linea.split(",")[5];
+	 * Equipo eqAux1= equipos.get(posEquipo(equipos, nomEq1));
+	 * Equipo eqAux2= equipos.get(posEquipo(equipos,nomEq2));
+	 * 
+	 * Pronostico aux = new
+	 * Pronostico(buscarPartido(partidos,eqAux1,eqAux2),eqAux1,resEquipo1(linea.
+	 * split(",")));
+	 * 
+	 * auxPart.agregarPronostico(aux);
+	 * }
+	 * }catch(Exception e) {
+	 * System.out.println(e);
+	 * }
+	 * 
+	 * }
+	 */
+
+	public static void leerBaseDatosPronosticos(ArrayList<String> configuraciones,
+			ArrayList<Participante> participantes, ArrayList<Equipo> equipos,
+			ArrayList<Partido> partidos) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection(configuraciones.get(0), configuraciones.get(1),
+					configuraciones.get(2));
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery("select participante,equipo1,resultado,equipo2 from pronosticos");
+			while (rs.next()) {
+				String nomPart = rs.getString(1);
+				Participante auxPart = new Participante(nomPart);
+				if (posParticipante(participantes, nomPart) != -1) {
+					auxPart = participantes.get(posParticipante(participantes, nomPart));
+				} else {
+					participantes.add(auxPart);
+				}
+				String nomEq1 = rs.getString(2);
+				String nomEq2 = rs.getString(4);
+				Equipo eqAux1 = equipos.get(posEquipo(equipos, nomEq1));
+				Equipo eqAux2 = equipos.get(posEquipo(equipos, nomEq2));
+
+				Pronostico aux = new Pronostico(buscarPartido(partidos, eqAux1, eqAux2), eqAux1,
+						resEquipo1(rs.getInt(3)));
+
+				auxPart.agregarPronostico(aux);
+			}
+
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+	public static boolean acertoPronosticosDeRonda(Participante part, Ronda ronda) {
+		boolean r = true;
+		ArrayList<Partido> partidosAcertados = new ArrayList<Partido>();
+		for (Pronostico p : part.pronosticosAcertados()) {
+			partidosAcertados.add(p.getPartido());
+		}
+		for (Partido p : ronda.getPartidos()) {
+			if (!partidosAcertados.contains(p)) {
+				r = false;
 			}
 		}
-		
+
 		return r;
-	}*/
+	}
+
+	public static void mostrarParticipante(Participante p) {
+		System.out.println("Participante: " + p.getNombre() + "\t" + "Puntos: " + p.puntosTotales() + "\t"
+				+ "Pronosticos acertados: " + p.pronosticosAcertados().size());
+	}
+
+	/*
+	 * public static boolean nombreADefinir(ArrayList<Ronda> rondas,Participante
+	 * part, String nroRonda) {
+	 * boolean r=true;
+	 * Ronda auxRonda= new Ronda("0");
+	 * for(Ronda ron:rondas) {
+	 * if(ron.getNro().equals(nroRonda)) {
+	 * auxRonda=ron;
+	 * }
+	 * }
+	 * 
+	 * return r;
+	 * }
+	 */
 }
--- src/main/java/programa/Main.java
+++ src/main/java/programa/Main.java
@@ -6,49 +6,61 @@ import java.util.ArrayList;
 import java.util.List;
 import java.nio.file.Files;
 
-
 public class Main {
 
     public static void main(String[] args) {
-        Path resultadosPath = Path.of("D:\\ProgramFilesx86\\workspace\\tpIntegrador\\src\\main\\java\\programa\\resultados");
-        Path configuracionesPath= Path.of("D:\\ProgramFilesx86\\workspace\\tpIntegrador_db\\src\\main\\java\\programa\\configuraciones");
+        Path resultadosPath = Path
+                .of("C:\\Users\\Fer\\eclipse-workspace\\Java_curso\\src\\tpIntegrador_db\\src\\main\\java\\programa\\resultados");
+        Path configuracionesPath = Path
+                .of("C:\\Users\\Fer\\eclipse-workspace\\Java_curso\\src\\tpIntegrador_db\\src\\main\\java\\programa\\configuraciones");
 
-        ArrayList<Ronda> rondas= new ArrayList<Ronda>();
+        ArrayList<Ronda> rondas = new ArrayList<Ronda>();
         ArrayList<Partido> partidos = new ArrayList<Partido>();
-        ArrayList<Equipo> equipos= new ArrayList<Equipo>();
-        ArrayList<Participante> participantes= new ArrayList<Participante>();
-        
-        String dbUrl="",dbUser="",dbPw="";
-        Integer puntosPorAcierto=0,puntosExtra=0;
-        
-        
-        Funciones.leerArchivoConfiguraciones(configuracionesPath,dbUrl, dbUser,dbPw,puntosPorAcierto, puntosExtra);        
-        
-        Funciones.leerArchivoResultados(resultadosPath,rondas,partidos,equipos);
-        
-        Funciones.leerBaseDatosPronosticos(dbUrl,dbUser,dbPw,participantes,equipos,partidos);
-       
-        // Imprimo los datos que deberian modificarse en la funcion "Funciones.leerArchivoConfiguraciones"
-        System.out.println(dbUrl);
-        System.out.println(dbUser);
-        System.out.println(dbPw);
-       
-        for(Participante p: participantes) {
-        	Funciones.mostrarParticipante(p);
+        ArrayList<Equipo> equipos = new ArrayList<Equipo>();
+        ArrayList<Participante> participantes = new ArrayList<Participante>();
+
+        ArrayList<String> configuraciones = new ArrayList<String>();
+        /* dbUrl = "", dbUser = "", dbPw = ""; */
+
+        Integer puntosPorAcierto = 0, puntosExtra = 0;
+
+        Funciones.leerArchivoConfiguraciones(configuracionesPath, configuraciones, puntosPorAcierto, puntosExtra);
+
+        Funciones.leerArchivoResultados(resultadosPath, rondas, partidos, equipos);
+
+        Funciones.leerBaseDatosPronosticos(configuraciones, participantes, equipos, partidos);
+
+        // Imprimo los datos que deberian modificarse en la funcion
+        // "Funciones.leerArchivoConfiguraciones"
+        System.out.println(configuraciones.get(0));
+        System.out.println(configuraciones.get(1));
+        System.out.println(configuraciones.get(2));
+        System.out.println(configuraciones.get(3));
+
+        for (Participante p : participantes) {
+            Funciones.mostrarParticipante(p);
         }
-        
-        //Funciones de system out para pruebas
-        
-       /* for(Partido p:partidos) {
-        	System.out.println(p.getEquipo1().getNombre()+" vs "+p.getEquipo2().getNombre());
-        }*/
-        
-        /*for(Ronda r: rondas) {
-        	System.out.println(r.getNro()+" "+Funciones.stringVs(r.getPartidos()));
-        }*/
-        
-        /*for(Partido p:partidos) {
-        	System.out.println("Ronda: "+p.getNroRonda()+"\t"+p.getEquipo1().getNombre()+" vs "+p.getEquipo2().getNombre());
-        }*/
+
+        // Funciones de system out para pruebas
+
+        /*
+         * for(Partido p:partidos) {
+         * System.out.println(p.getEquipo1().getNombre()+" vs "+p.getEquipo2().getNombre
+         * ());
+         * }
+         */
+
+        /*
+         * for(Ronda r: rondas) {
+         * System.out.println(r.getNro()+" "+Funciones.stringVs(r.getPartidos()));
+         * }
+         */
+
+        /*
+         * for(Partido p:partidos) {
+         * System.out.println("Ronda: "+p.getNroRonda()+"\t"+p.getEquipo1().getNombre()
+         * +" vs "+p.getEquipo2().getNombre());
+         * }
+         */
     }
 }
